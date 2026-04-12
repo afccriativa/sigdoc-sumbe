30518444d21f38750f1ffec0f7de88cf4e5d63c9
afccriativa <augustofaztudo4@gmail.com>
2026-04-12 08:04:47 +0100

    Update guia-medica-builder.js

--- guia-medica-builder.js
+++ guia-medica-builder.js
@@ -235,7 +235,8 @@ padding: 2px;
           </div>
         </div>
 
-        <img class="gm-logo-rodape" src="Imagem1."/>
+      <img class="gm-logo-rodape" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Logo_Governo_de_Angola.png/640px-Logo_Governo_de_Angola.png" alt="Governo de Angola"/>
+      </div>
 
             <!-- PÁGINA 2 -->
       <div class="gm-pagina">
@@ -306,7 +307,8 @@ padding: 2px;
           </div>
         </div>
         
-        <img class="gm-logo-rodape" src="Imagem1.png"/>
+        <img class="gm-logo-rodape" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Logo_Governo_de_Angola.png/640px-Logo_Governo_de_Angola.png" alt="Governo de Angola"/>
+      </div>
       </div>
     </div>
   `;
